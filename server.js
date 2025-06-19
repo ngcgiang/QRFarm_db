286a57242b36e5c18fad9e33a9e29af1b7c3c65e
Nguyễn Ngọc Giang <ngocgiang1832004@gmail.com>
2025-06-19 14:41:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://qrfarmtracking.netlify.app/']
+  origin: ['http://localhost:3000', 'https://qrfarmtracking.netlify.app']
 }));
 app.use(express.json());
 
